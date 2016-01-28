7a79368497964849ae40e6d46bdf2a1d8fa7d08a
Ondrej Jamriska <ondrej.jamriska@gmail.com>
2016-01-28 23:52:38 +0100

    add public domain dedication

--- jzq.h
+++ jzq.h
@@ -1,3 +1,7 @@
+// This software is in the public domain. Where that dedication is not
+// recognized, you are granted a perpetual, irrevocable license to copy
+// and modify this file as you see fit.
+
 #ifndef JZQ_H_
 #define JZQ_H_
 
